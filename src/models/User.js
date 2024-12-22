82e19e5df89725a88c7f8ba6783866b8de04cb88
drashtipatel01 <drashtipatel11422@gmail.com>
2024-12-22 14:27:42 +0530

    confirm pwd

--- src/models/User.js
+++ src/models/User.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
-    name: { type: String, required: true },
+    name: { type: String, required: false },
     password: { type: String, required: true },
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now }
