ce36b4f782c9ae8482a6646b3057b60a775b6163
drashtipatel01 <drashtipatel11422@gmail.com>
2024-12-22 15:57:18 +0530

    added swagger

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const connectDB = require('./config/db');
+const swaggerUi = require('swagger-ui-express');
+const swaggerFile = require('../swagger_output.json');
 const attendanceRoutes = require('./routes/attendanceRoutes');
 const authRoutes = require('./routes/authRoutes'); 
 
@@ -21,6 +23,10 @@ app.get('/api', (req, res) => {
     res.send('You are connected to server');
 });
 const PORT = process.env.PORT || 5000;
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
+    console.log(`Swagger docs available at http://localhost:${5000}/api-docs`);
+
 });
